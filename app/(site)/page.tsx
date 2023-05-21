89a32af722faa65cf5b271e6f5ebf80bb2a7a34e
Derek <derekjhtong@gmail.com>
2023-05-21 15:52:20 -0400

    Authentication and Logout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,7 +12,8 @@ export default function Home() {
         py-12
         sm:px-6
         lg:px-8
-        bg-gray-100"
+        bg-gray-100
+        "
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
