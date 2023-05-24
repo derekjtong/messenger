788a68b6fca9a4dd90a2fe53bdb70590c9a57c4f
Derek <derekjhtong@gmail.com>
2023-05-24 16:54:34 -0400

    Added Conversation Info

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -3,10 +3,9 @@ import { useMemo } from "react";
 import { FullConversationType } from "../types";
 import { User } from "@prisma/client";
 
-const useOtherUser = (conversation: FullConversationType | {
-  users: User[]
-}) => {
+const useOtherUser = (conversation: FullConversationType | {users: User[]}) => {
   const session = useSession();
+  
   const otherUser = useMemo(() => {
     const currentUserEmail = session?.data?.user?.email;
 
